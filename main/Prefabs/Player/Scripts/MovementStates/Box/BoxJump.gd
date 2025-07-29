0c7cfb96f872a02798a25c334ef5432adb0a12c6
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-29 14:37:30 +1200

    + Particles

--- main/Prefabs/Player/Scripts/MovementStates/Box/BoxJump.gd
+++ main/Prefabs/Player/Scripts/MovementStates/Box/BoxJump.gd
@@ -1,16 +1,30 @@
 extends State
 
+@onready var jump_cd: Timer = %JumpCD
+
 @export_group("Box Movement")
 @export var BoxMove: State
 @export var BoxIdle: State
 
 @export_group("Stats")
 @export var jump_power := 5
+@export var jump_amount := 2
+@export var can_jump := true
+
 func enter() -> void:
 	print("Box: Jump")
-	parent.apply_impulse(parent.basis.y * 5)
-	parent.angular_velocity = Vector3.ZERO
+	
+	if can_jump and jump_amount > 0:
+		parent.apply_impulse(parent.basis.y * jump_power)
+		parent.angular_velocity = Vector3.ZERO
+		jump_amount -= 1
+		print("Jumped. Remaining:", jump_amount)
 
+		if jump_amount == 0:
+			can_jump = false
+			jump_cd.start()
+	else:
+		print("Jump on cooldown.")
 func exit() -> void:
 	pass
 
@@ -18,8 +32,6 @@ func exit() -> void:
 func process_input(event: InputEvent) -> State:
 	if Input.get_vector("move_left","move_right","move_forward","move_back"):
 		return BoxMove
-	else:
-		return BoxIdle
 	return null
 
 func process_frame(delta: float) -> State:
@@ -28,3 +40,9 @@ func process_frame(delta: float) -> State:
 ## Function that dictates what this movement state does
 func process_physics(delta: float) -> State:
 	return null
+
+
+func _on_jump_cd_timeout() -> void:
+	jump_amount = 2
+	can_jump = true
+	print("Jump reset.")
